332fc1455488ea3aaff3d0ffbe6e0207cb227dd7
Timothy Lowe <TimothyLowe247@gmail.com>
2019-05-25 05:01:32 -0400

    worked out the basic logic for the game will bug fix in morning and move onto Renew

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -9,7 +9,9 @@ const {
  	diceBox,
  	playerInfoBox,
  	container,
- 	die 
+ 	die,
+ 	diceText,
+ 	active 
  } = gameBoardStyles
 
 const ScoreBoard = props => {
@@ -18,41 +20,51 @@ const ScoreBoard = props => {
 	return (
 		<div className={scoreBoard}>
 			<div className={player}>
-				<h3>{player1}</h3>
+				<h3>{player1}'s Past Wins</h3>
 			</div>
 			<div className={goalBox}>
-				<h3>The Goal</h3>
+				<h4>The Goal</h4>
 				<h3>{goal}</h3>
 			</div>
 			<div className={player}>
-				<h3>{player2}</h3>
+				<h3>{player2}'s Past Wins</h3>
 			</div>
 		</div>
 	)
 }
 const Dice = props => {
-	const {die1, die2} = props
+	const {die1, die2, rollDice} = props
 	return (
-		<div className={diceBox}>
+		<div 
+		className = {diceBox}
+		onClick = {rollDice}
+		>
 			<div className={die}>
 				{die1}
 			</div>
 			<div className={die}>
 				{die2}
 			</div>
+			<h3 className={diceText}>
+			Click to roll Dice
+			</h3>
 		</div>
 		)
 }
 const PlayerInfo = props =>{
-	const {playerRolls, player}= props
+	const {playerRolls, player, currentPlayer, score}= props
+	const indiactor = player === currentPlayer? active: null
 	return(
-		<div className={playerInfoBox}>
-			<h1>{player}</h1>
-			{playerRolls.map((roll, key)=>{
-				return(
-					<li key={key}>{roll}</li>
-					)
-			})}
+		<div className= {indiactor}>
+			<div className={playerInfoBox}>
+				<h2>{player}'s score {score}</h2>
+				<h5>{player}'s rolls</h5>
+				{playerRolls.map((roll, key)=>{
+					return(
+						<li key={key}>{roll}</li>
+						)
+				})}
+			</div>
 		</div>
 		)
 }
@@ -65,7 +77,12 @@ const GameBoard = props => {
 		die1, 
 		die2,
 		player1Rolls,
-        player2Rolls 
+        player2Rolls,
+        rollDice,
+        currentPlayer, 
+        active,
+        player1Score,
+        player2Score
 	} = props
 	return (
 		<div className={background}>
@@ -76,16 +93,21 @@ const GameBoard = props => {
 			/>
 			<div className = {container}>
 				<PlayerInfo
+					score={player1Score}
 					player = {player1}
 					playerRolls= {player1Rolls}
+					currentPlayer = {currentPlayer}
 				/>
 				<Dice 
+					rollDice = {rollDice}
 					die1 = {die1}
 	           		die2 = {die2}
 				/>
 				<PlayerInfo
+					score={player2Score}
 					player = {player2}
             		playerRolls= {player2Rolls}
+            		currentPlayer = {currentPlayer}
 				/>
 			</div>
 		
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,21 +17,198 @@ class IndexPage extends React.Component {
           player2: "player2",
           player1Rolls: [],
           player2Rolls: [],
+          player1Score: 0,
+          player2Score: 0,
+          currentPlayer: null,
           goal: 0,
           die1: 0,
-          die2: 0
+          die2: 0,
+          turnCount:0
         };
         this.startGame = this.startGame.bind(this);
+        this.setPlayerName = this.setPlayerName.bind(this);
+        this.initializeCurrentPlayer = this.initializeCurrentPlayer.bind(this);
+        this.handleStartGame = this.handleStartGame.bind(this);
+        this.whereIsGoal = this.whereIsGoal.bind(this);
+        this.rollDice = this.rollDice.bind(this);
+        this.diceVal = this.diceVal.bind(this);
+        this.setCurrentPlayer = this.setCurrentPlayer.bind(this);
+        this.calcPlayerScore = this.calcPlayerScore.bind(this);
+        this.checkWin = this.checkWin.bind(this);
     }
+
     startGame(){
       this.setState(prevState=>{
         prevState.toggleGameboard = !prevState.toggleGameboard
         return prevState;
       })
     }
-    
+    initializeCurrentPlayer(){
+      console.log("initializeCurrentPlayer")
+      const {turnCount, currentPlayer, player1, player2} = this.state
+      if(turnCount< 2){
+
+          if (turnCount % 2 === 0) {
+            this.setState({currentPlayer: player1}, ()=> this.setPlayerName())
+          } 
+          else {
+            this.setState({currentPlayer: player2}, ()=> this.setPlayerName())
+          }
+      }
+      else{
+       this.whereIsGoal()
+      }
+    }
+    randomOnetoSix(){
+      return Math.floor(1 + Math.random() * 6);
+    }
+    whereIsGoal(){
+       let setGoal = prompt('Input a Positive number to reach with dice rolls');
+      if (isNaN(setGoal)) {
+        alert('Please Select A Positive integer');
+        this.whereIsGoal()
+
+      } else if (setGoal <= 0) {
+        alert('Please Select A Positive integer');
+        this.whereIsGoal();
+
+
+      } else if (!setGoal) {
+        alert('Please Select A Positive integer');
+        this.whereIsGoal();
+
+
+      } else {
+        setGoal = Number(setGoal);
+        // console.log("GOAL IS SET TO " + setGoal);
+        // $('#goal').text(`THE GOAL:${setGoal}`);
+        this.setState({goal:setGoal})
+         // game.currentGoal = setGoal;
+        // const dice = document.querySelector('#diceHolder');
+        // dice.addEventListener('click', rollDice)
+      }
+    }
+    setPlayerName(){
+      console.log("setPlayerName")
+
+          const {currentPlayer, player1, player2, turnCount} = this.state
+          const name = prompt(`Please enter ${currentPlayer}'s name!`)
+          this.setState(prevState=>{
+            prevState.turnCount++
+            prevState[currentPlayer] = name
+            return prevState
+
+          },()=> this.initializeCurrentPlayer())      
+    }
+    rollDice(){
+      console.log("rollDice")
+      this.diceVal();
+    }
+    setCurrentPlayer(total, die1, die2){
+       const {
+         currentPlayer, 
+         player1, 
+         player2, 
+         turnCount, 
+         player1Rolls, 
+         player2Rolls,
+       } = this.state
+        if (turnCount % 2 === 0) {
+              this.setState(prevState=> {
+              console.log("player1", this.state)
+              prevState.player1Rolls.push(total)
+              prevState.currentPlayer = prevState.player1
+              prevState.die1 = die1
+              prevState.die2 = die2
+              prevState.turnCount++
+              return prevState
+            }, ()=>{
+              this.calcPlayerScore(player1, player1Rolls);
+            })
+        } else {
+          this.setState(prevState=> {
+              console.log("player2", this.state)
+              prevState.player2Rolls.push(total)
+              prevState.currentPlayer = prevState.player2
+              prevState.die1 = die1
+              prevState.die2 = die2
+              prevState.turnCount++
+              return prevState
+            }, ()=>{
+              this.calcPlayerScore(player2, player2Rolls);
+            })
+        }
+    }
+    diceVal(){
+      const {
+        die1,
+        die2, 
+        currentPlayer, 
+        player1, 
+        player1Rolls, 
+        player2Rolls
+      } = this.state
+
+      const firstDie = this.randomOnetoSix();
+      const secondDie = this.randomOnetoSix();
+      const total = firstDie + secondDie;
+      console.log("diceVal Check", firstDie, secondDie);
+      this.setCurrentPlayer(total, firstDie, secondDie);
+      
+
+    }
+    handleStartGame(){
+      this.initializeCurrentPlayer()
+      this.startGame();
+    }
+    calcPlayerScore(player, playerRolls){
+      // const currentRolls = player1Rolls;
+      const {player1, player2, player1Score, player2Score} = this.state
+      let total = 0;
+      for (let i = 0; i < playerRolls.length; i++) {
+        total += playerRolls[i];
+      }
+      if(player === player1){
+          console.log("HERE1")
+
+        this.setState(prevState=> {
+          prevState.player1Score+=total
+          return prevState;
+        }, ()=> {
+          console.log("HERE")
+          this.checkWin(player1, player1Score)
+        })
+      }
+      if(player === player2){
+          console.log("HERE1")
+
+        this.setState(prevState=> {
+          prevState.player2Score+=total
+          return prevState;
+        }, ()=> {
+          console.log("HERE")
+
+          this.checkWin(player2, player2Score)
+        })
+      }
+    }
+    checkWin(player, playerScore){
+
+      const {goal} = this.state
+      console.log(`CheckingWin ${playerScore}>=${goal}?`)
+
+      if (playerScore >= goal) {
+      console.log(`${player} wins`)
+
+        const playAgain = confirm(`${player} wins! play again?`);
+      }
+    }
     render() {
-      const {startGame} = this
+      const {
+        startGame,
+        handleStartGame,
+        rollDice
+      } = this
       const {
         toggleGameboard, 
         player1, 
@@ -40,13 +217,17 @@ class IndexPage extends React.Component {
         die1, 
         die2,
         player1Rolls,
-        player2Rolls
+        player2Rolls,
+        currentPlayer,
+        player1Score,
+        player2Score
+        
       } = this.state 
 
-      
+      console.log("index state", this.state)
       return (
         <div>
-        { toggleGameboard ===true ? 
+        { toggleGameboard === true ? 
           <GameBoard 
             player1 = {player1} 
             player2 = {player2}
@@ -55,9 +236,14 @@ class IndexPage extends React.Component {
             die2 = {die2}
             player1Rolls= {player1Rolls}
             player2Rolls= {player2Rolls}
-
+            rollDice = {rollDice}
+            currentPlayer = {currentPlayer}
+            player1Score={player1Score}
+            player2Score={player2Score}
           /> :
-          <ReachTheGoal handleClick={startGame}/>  }
+          <ReachTheGoal 
+            handleClick={handleStartGame}
+          />  }
 
         </div>
       )
@@ -65,13 +251,3 @@ class IndexPage extends React.Component {
   }
 
 export default IndexPage
-// <Layout>
-//     <SEO title="Home" />
-//     <h1>Hi people</h1>
-//     <p>Welcome to your new Gatsby site.</p>
-//     <p>Now go build something great.</p>
-//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-//       <Image />
-//     </div>
-//     <Link to="/page-2/">Go to page 2</Link>
-//   </Layout>
